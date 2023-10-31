008c25880184f18eba43f6709dca0d98792b8b8e
prem-code-dot <prem.21910524@viit.ac.in>
2023-10-31 22:52:10 +0530

    Moved from String to Array Folder

---
+++ Array/SortEleByNoOf1Bit.java
@@ -0,0 +1,27 @@
+// Sort Integers by The Number of 1 Bits in given array
+
+package Array;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortEleByNoOf1Bit {
+    public static void main(String[] args) {
+        Integer arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Arrays.sort(arr, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer num1, Integer num2) {
+                Integer c1 = Integer.bitCount(num1);
+                Integer c2 = Integer.bitCount(num2);
+                if (c1 == c2)
+                    return Integer.compare(num1, num2);
+                else
+                    return Integer.compare(c1, c2);
+            }
+        });
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
