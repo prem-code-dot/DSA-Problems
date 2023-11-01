f8a90d5b3736f806d83636d32acd17afad086d4b
prem-code-dot <prem.21910524@viit.ac.in>
2023-11-01 20:12:59 +0530

    Implementation of Binary Search Tree

---
+++ Tree/BST.java
@@ -0,0 +1,79 @@
+//implementation of binary search tree
+class Node {
+	int data;
+	Node left;
+	Node right;
+
+	public Node(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+public class BST {
+	static Node root;
+
+	public void insert(int data) {
+		Node newNode = new Node(data);
+		if (root == null) {
+			root = newNode;
+		} else {
+			insert(root, newNode);
+		}
+	}
+
+	private static void insert(Node root, Node newNode) {
+		if (root.data > newNode.data) {
+			if (root.left == null) {
+				root.left = newNode;
+			} else {
+				insert(root.left, newNode);
+			}
+		} else // (root.data >= newNode.data)
+		{
+			if (root.right == null) {
+				root.right = newNode;
+			} else {
+				insert(root.right, newNode);
+			}
+		}
+	}
+
+	public void print() {
+		if (root == null) {
+			System.out.println("Empty Tree");
+		} else {
+			print(root);
+		}
+	}
+
+	private void print(Node root) {
+		// TODO Auto-generated method stub
+		if (root == null) {
+			return;
+		}
+		print(root.left);
+		print(root.right);
+		System.out.print(root.data + " ");
+
+		// System.out.println(root.data);
+		// print(root);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BST t = new BST();
+		t.insert(3);
+		t.insert(2);
+		t.insert(1);
+		t.insert(4);
+		t.insert(5);
+		t.insert(6);
+
+		t.print();
+
+	}
+
+}
