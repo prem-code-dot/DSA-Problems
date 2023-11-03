38dee4d653139a4c8bcac5477c3fd1376586bbc2
prem-code-dot <prem.21910524@viit.ac.in>
2023-11-04 00:55:33 +0530

    Convert Roman To Integer

---
+++ Array/RomanToInteger.java
@@ -0,0 +1,38 @@
+// Given a roman numeral, convert it to an integer.
+
+package Array;
+
+import java.util.HashMap;
+
+public class RomanToInteger {
+
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int n = s.length();
+
+        int ans = map.get(s.charAt(n - 1));
+        for (int i = n - 2; i >= 0; i--) {
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                ans = ans - map.get(s.charAt(i));
+            } else
+                ans += map.get(s.charAt(i));
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        int n = romanToInt(s);
+        System.out.println(n);
+    }
+
+}
